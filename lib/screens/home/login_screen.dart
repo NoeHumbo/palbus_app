88974dcbb0e2c8dd9efbdf402b97631af4dc4de8
SnakeWD <73128821+SnakeWD@users.noreply.github.com>
2020-10-23 10:50:08 -0500

    Add files via upload

--- lib/screens/home/login_screen.dart
+++ lib/screens/home/login_screen.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'package:palbus_app/screens/home/home_screen.dart';
 
@@ -22,7 +23,9 @@ class _LoginScreenState extends State<LoginScreen>
   RegExp emailRegExp =
       new RegExp(r'^\w+[\w-\.]*\@\w+((-\w+)|(\w*))\.[a-z]{2,3}$');
   RegExp contRegExp = new RegExp(r'^([1-zA-Z0-1@.\s]{1,255})$');
+  // ignore: unused_field
   String _correo;
+  // ignore: unused_field
   String _contrasena;
   String mensaje = '';
 
@@ -47,103 +50,105 @@ class _LoginScreenState extends State<LoginScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: _logueado ? HomeScreen() : loginForm(),
-      //body: loginForm(),
+     body: _logueado ? HomeScreen(mensaje: mensaje) : loginForm(),
+     //body: loginForm(),
     );
   }
 
   Widget loginForm() {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: <Widget>[
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            SizedBox(
-              height: 100.0,
-            ),
+    return SingleChildScrollView(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+            SizedBox(height: 100.0,),
             Flexible(
               flex: 2,
               child: SafeArea(
                 child: FractionallySizedBox(
                   widthFactor: 0.7,
                   child: Image.asset('assets/logo-1.png'),
+
                 ),
               ),
             ),
-          ],
-        ),
-        Container(
-          width: 300.0, //size.width * .6,
-          child: Form(
-            key: _key,
-            child: Column(
-              children: <Widget>[
-                TextFormField(
-                  validator: (text) {
-                    if (text.length == 0) {
-                      return "Este campo correo es requerido";
-                    } else if (!emailRegExp.hasMatch(text)) {
-                      return "El formato para correo no es correcto";
-                    }
-                    return null;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  maxLength: 50,
-                  textAlign: TextAlign.center,
-                  decoration: InputDecoration(
-                    hintText: 'Ingrese su Correo',
-                    labelText: 'Correo',
-                    counterText: '',
-                    icon:
-                        Icon(Icons.email, size: 32.0, color: Colors.blue[800]),
+            ],
+          ),
+          Container(
+            width: 300.0, //size.width * .6,
+            child: Form(
+              key: _key,
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    validator: (text) {
+                      if (text.length == 0) {
+                        return "Este campo correo es requerido";
+                      } else if (!emailRegExp.hasMatch(text)) {
+                        return "El formato para correo no es correcto";
+                      }
+                      return null;
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    maxLength: 50,
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      hintText: 'Ingrese su Correo',
+                      labelText: 'Correo',
+                      counterText: '',
+                      icon:
+                          Icon(Icons.email, size: 32.0, color: Colors.blue[800]),
+                    ),
+                    onSaved: (text) => _correo = text,
                   ),
-                  onSaved: (text) => _correo = text,
-                ),
-                TextFormField(
-                  validator: (text) {
-                    if (text.length == 0) {
-                      return "Este campo contraseña es requerido";
-                    } else if (text.length <= 5) {
-                      return "Su contraseña debe ser al menos de 5 caracteres";
-                    } else if (!contRegExp.hasMatch(text)) {
-                      return "El formato para contraseña no es correcto";
-                    }
-                    return null;
-                  },
-                  keyboardType: TextInputType.text,
-                  maxLength: 20,
-                  textAlign: TextAlign.center,
-                  decoration: InputDecoration(
-                    hintText: 'Ingrese su Contraseña',
-                    labelText: 'Contraseña',
-                    counterText: '',
-                    icon: Icon(Icons.lock, size: 32.0, color: Colors.blue[800]),
+                  TextFormField(
+                    obscureText: true,
+                    validator: (text) {
+                      if (text.length == 0) {
+                        return "Este campo contraseña es requerido";
+                      } else if (text.length <= 5) {
+                        return "Su contraseña debe ser al menos de 5 caracteres";
+                      } else if (!contRegExp.hasMatch(text)) {
+                        return "El formato para contraseña no es correcto";
+                      }
+                      return null;
+                    },
+                    keyboardType: TextInputType.text,
+                    maxLength: 20,
+                    textAlign: TextAlign.center,
+                    decoration: InputDecoration(
+                      hintText: 'Ingrese su Contraseña',
+                      labelText: 'Contraseña',
+                      counterText: '',
+                      icon: Icon(Icons.lock, size: 32.0, color: Colors.blue[800]),
+                    ),
+                    onSaved: (text) => _contrasena = text,
                   ),
-                  onSaved: (text) => _contrasena = text,
-                ),
-                IconButton(
-                  onPressed: () {
-                    if (_key.currentState.validate()) {
-                      _key.currentState.save();
-                      //Aqui se llamaria a su API para hacer el login
-                      setState(() {
-                        _logueado = true;
-                      });
-                      Navigator.of(context).pushReplacementNamed('app_init');
-                    }
-                  },
-                  icon: Icon(
-                    Icons.arrow_forward,
-                    size: 42.0,
-                    color: Colors.blue[800],
-                  ),
-                )
-              ],
+                  IconButton(
+                    onPressed: () {
+                      if (_key.currentState.validate()) {
+                        _key.currentState.save();
+                        //Aqui se llamaria a su API para hacer el login
+                        setState(() {
+                          _logueado = true;
+                        });
+                        Navigator.of(context).push(HomeScreen.route(mensaje));
+                      }
+                    },
+                    icon: Icon(
+                      Icons.arrow_forward,
+                      size: 42.0,
+                      color: Colors.blue[800],
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 }
